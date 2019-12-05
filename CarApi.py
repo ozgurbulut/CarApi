ff9ec1e6597470d3a7a0080aed173044d784c55c
Özgür Bulut <ozgurbulut0@outlook.com>
2019-12-05 10:39:27 +0300

    Update CarApi.py

--- CarApi.py
+++ CarApi.py
@@ -1,12 +1,15 @@
 url = 'https://www.cars.com/for-sale/searchresults.action/?dealerType=localOnly&page=1&perPage=20&searchSource=GN_BREADCRUMB&sort=relevance&zc=90006'
-import flask
+from flask import Flask
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app = Flask(__name__)
 
-
-@app.route('/', methods=['GET'])
+@app.route("/")
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-
-app.run()
+    return "Hello, World!"
+    
+@app.route("/cars/list/extcolor=black")
+def salvador():
+    return "Hello, siyah araba"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
