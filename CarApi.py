091831286d575a69f9ccd8e63d1cbc4563b89a80
Özgür Bulut <ozgurbulut0@outlook.com>
2019-12-06 10:07:42 +0300

    Update CarApi.py

--- CarApi.py
+++ CarApi.py
@@ -28,3 +28,4 @@ https://uniwebsidad.com/libros/explore-flask/chapter-6/url-converters
 #Onemli ? isaretini buldum
 https://pythonise.com/series/learning-flask/flask-query-strings
 https://www.digitalocean.com/community/tutorials/how-to-scrape-web-pages-with-beautiful-soup-and-python-3
+    https://www.digitalocean.com/community/tutorials/how-to-scrape-web-pages-with-beautiful-soup-and-python-3
